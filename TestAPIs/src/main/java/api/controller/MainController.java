cbd1561875ca49a9ff3788bde8d1e91cdab5de62
praharsh <praharsh.bist@snapdeal.com>
2016-11-25 14:23:47 +0530

    first commit

---
+++ TestAPIs/src/main/java/api/controller/MainController.java
@@ -0,0 +1,32 @@
+package api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import api.service.MainService;
+
+@RestController
+public class MainController {
+	
+	@Autowired
+	MainService service;
+	
+	@RequestMapping(value = "/getCall", method = RequestMethod.GET)
+	@ResponseBody
+	public String getCall()
+	{
+		return this.service.getCall();
+	}
+	
+	@RequestMapping(value = "/resetHystrix", method = RequestMethod.GET)
+	@ResponseBody
+	public String resetHystrix()
+	{
+		return this.service.resetHystrix();
+	}
+	
+
+}
---
+++ TestAPIs/src/main/java/api/external/ExternalSim.java
@@ -0,0 +1,33 @@
+package api.external;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExternalSim {
+	
+
+	private long wait;
+	
+	public ExternalSim() {
+		super();
+	}
+	
+	public ExternalSim(long wait) {
+		this.wait = wait;
+	}
+	
+	public void setWait(int n)
+	{
+		wait = n;
+	}
+	
+	public String getExternalData() throws InterruptedException
+	{
+		System.out.println("Called external");
+		Thread.sleep(wait);
+		/*System.out.println("returned Response");*/
+        return "Success";
+	}
+
+
+}
